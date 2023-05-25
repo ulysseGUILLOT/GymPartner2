84fc5ee8470453699acc8ce7232bab5209405d9d
ulysseGUILLOT <ulysseguillot@gmail.com>
2023-05-25 15:44:35 +0200

    changement architecture fichiers (modèles / routes)

--- src/bp_exercices.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request, jsonify
-
-bp_exercises = Blueprint(__name__, 'bp_exercises', url_prefix="exercises")
-
-
-@bp_exercises.route("/all")
-def get_all():
-    return 'exercises list'
---
+++ src/config_db.py
@@ -0,0 +1,16 @@
+import os
+
+
+def config_db(app):
+    # Récupération des variables d'environnement
+    db_host = os.environ.get('DB_HOST')
+    db_name = os.environ.get('DB_NAME')
+    db_port = os.environ.get('DB_PORT')
+    db_user = os.environ.get('DB_USER')
+    db_password = os.environ.get('DB_PASSWORD')
+
+    # Création de la chaine de caractère de connexion sql
+    db_info = 'mysql+pymysql://' + db_user + ':' + db_password + '@' + db_host + ':' + db_port + '/' + db_name
+
+    # Configuration des paramètres de connexion de la DB
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_info
